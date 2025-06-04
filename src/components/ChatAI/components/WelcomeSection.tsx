92ba60b268d4b811091ce78a7e6b56240bf5a9c4
Hieu Nguyen <godjungle0908@gmail.com>
2025-06-05 00:56:03 +0700

    intital

--- src/components/ChatAI/components/WelcomeSection.tsx
+++ src/components/ChatAI/components/WelcomeSection.tsx
@@ -10,6 +10,7 @@ export const WelcomeSection: React.FC<WelcomeSectionProps> = ({
   welcomeText,
   welcomeDescription,
 }) => {
+  console.log("123");
   return (
     <motion.div
       className="text-center mb-6"
